91057bcdee6e7af5e278ab994f9cf2390379ee49
walacecordeiro <walacecordeirodossantos@gmail.com>
2023-02-15 18:30:47 -0300

    creating HTML with JavaScript

--- js/favorites.js
+++ js/favorites.js
@@ -1,26 +1,78 @@
 export class Favorites {
-    constructor(root){
+    constructor(root) {
         this.root = document.querySelector(root)
+        this.load()
+    }
+
+    load() {
+        this.entries = [{
+            login: 'walacecordeiro',
+            name: "Walace Cordeiro",
+            public_repos: '95',
+            followers: '12000',
+        },
+        {
+            login: 'diego3g',
+            name: "Diego Fernandes",
+            public_repos: '95',
+            followers: '12000',
+        },
+        ]
+    
     }
 }
 
-export class FavoritesView extends Favorites{
-    constructor(root){
+export class FavoritesView extends Favorites {
+    constructor(root) {
         super(root)
+
+        this.tbody = this.root.querySelector('table tbody')
 
         this.update()
     }
 
-    update(){
-       this.removeAllTr()
+    update() {
+        this.removeAllTr()
+
+        this.entries.forEach( user => {
+            const row = this.createRow()
+
+            row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+            row.querySelector('.user img').alt = `Imagem de ${user.name}`
+            row.querySelector('.user p').textContent = user.name
+            row.querySelector('.user span').textContent = user.login
+            row.querySelector('.repositories').textContent = user.public_repos
+            row.querySelector('.followers').textContent = user.followers
+
+            this.tbody.append(row)
+        })
     }
 
-    removeAllTr(){
-        const tbody = this.root.querySelector('table tbody')
+    createRow() {
+        const tr = document.createElement('tr')
 
-        tbody.querySelectorAll('tr')
-        .forEach((tr) => {
-            tr.remove()
-        })
+        tr.innerHTML = `
+            <td class="user">
+              <img src="https://github.com/walacecordeiro.png" alt="" />
+              <a href="https://github.com/walacecordeiro" target="_blank">
+                <p>Walace Cordeiro</p>
+                <span>Wal-le</span>
+              </a>
+            </td>
+            <td class="repositories">76</td>
+            <td class="followers">98465</td>
+            <td>
+              <button class="remove">Remover</button>
+            </td>
+       `
+        return tr
+    }
+
+    removeAllTr() {
+
+        this.tbody.querySelectorAll('tr')
+            .forEach((tr) => {
+                tr.remove()
+            })
     }
 }
